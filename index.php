e0ee43975e5c873b257b235cb83943057078d351
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-26 15:46:21 +0200

    add files and print data hotels array

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+$hotels = [
+
+  [
+    'name' => 'Hotel Belvedere',
+    'description' => 'Hotel Belvedere Descrizione',
+    'parking' => true,
+    'vote' => 4,
+    'distance_to_center' => 10.4
+  ],
+  [
+    'name' => 'Hotel Futuro',
+    'description' => 'Hotel Futuro Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 2
+  ],
+  [
+    'name' => 'Hotel Rivamare',
+    'description' => 'Hotel Rivamare Descrizione',
+    'parking' => false,
+    'vote' => 1,
+    'distance_to_center' => 1
+  ],
+  [
+    'name' => 'Hotel Bellavista',
+    'description' => 'Hotel Bellavista Descrizione',
+    'parking' => false,
+    'vote' => 5,
+    'distance_to_center' => 5.5
+  ],
+  [
+    'name' => 'Hotel Milano',
+    'description' => 'Hotel Milano Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 50
+  ],
+
+];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Titolo</title>
+
+  <!-- LINK VUE.JS -->
+  <!-- <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script> -->
+
+  <!-- LINK BOOTSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+
+  <!-- LINK MY CSS -->
+  <link rel="stylesheet" href="./css/style.css" />
+
+  <!-- SCRIPT MY JAVASCRIPT -->
+  <script src="./js/main.js" defer></script>
+</head>
+
+<body>
+  <?php foreach ($hotels as $hotel): ?>
+  <ul>
+
+    <?php foreach ($hotel as $key => $value): ?>
+    <li>
+      <?= $key; ?>:
+      <?= $value; ?>
+    </li>
+    <?php endforeach; ?>
+
+  </ul>
+  <?php endforeach; ?>
+
+
+  <!-- DIV VUE.JS -->
+  <!-- <div id="app">{{ message }}</div> -->
+  <!-- SCRIPT BOOTSTRAP JAVASCRIPT -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,53 @@
+* Domande esercizio 
+
+- mancano immagini?
+  - immagine testimonials?
+  - immagine disordinata
+  - immagine footer
+
+- I data metterli in un file data statici
+
+- Slider testimonials
+  - 2 immagini con position absolute e z-index
+  - un immagine con background con una variabile da incerire nel css computed
+  - l'immagini di testimonials c'è solo hello? e le scritte sono a mano?
+
+- Due bottoncini affianco?? ignorare
+
+- Immagine disordinata
+  - esiste una forma per farlo??
+  - scritta ma scritta a mano??
+
+- Choose pizza è un carousel? immagini sfuocate?
+
+- Book table, funzionamento?
+ helvetica
+- **Bonus:** per trovare il font gusto?
+
+**Programma**
+
+- finire layout (due sezioni frasi)
+- trasferire tutti i dati statici in un file
+- classe bottone
+- classe input book table
+
+- botton return up
+- variante bck ground con css
+- funzionamento caroesello
+- migliorare CSS
+
+
+- Layout 
+- pizzeria
+- diviso in tre
+- strutture che si ripetono
+- struttura 2 dettagli diversi componenti
+- a differenza di listg card
+- variabile di allarme
+- come la maggior parte del sito dat in un file
+- punto positivo dati statici per modifiche aprimiamo solo un file
+- esepio parte select
+
+
+uso git
+
